3f8a6a50d40d9ecf1871787f58b1bde931c03f37
jingwei gu <sirgujingwei@gmail.com>
2011-09-25 11:45:23 -0400

    hw2

--- src/travel/travel.cpp
+++ src/travel/travel.cpp
@@ -7,20 +7,67 @@ travelling salesman  puzzles
 #include <string>
 #include <fstream>
 #include <vector>
+#include <set>
+#include <deque>
+#include <math.h>
 
 struct coor
 {
-  int x;
-  int y;
-  int z; 
+  float x;
+  float y;
+  float z; 
 };
 
+std::vector<coor> city;
 
+float calCost(const coor &a, const coor &b)
+{
+  // calculate the distance between two points
+  return sqrt( pow(a.x - b.x, 2)+pow(a.y-b.y, 2)+pow(a.z-b.z,2) );
+}
+
+float calQue(const std::deque<int> &que)
+{
+  // get the total cost in the queue
+  float sum = 0;
+  for(int i=0;i!=que.size()-1;i++){
+    sum += calCost(city[que[i]-1],city[que[i+1]-1]);
+  }
+  return sum;
+}
+
+
+int c=0;
+
+void f(const std::deque<int> &que,const std::set<int> &s)
+{
+  if (s.empty()){
+    for(int i=0;i!=que.size();i++){
+      std::cout << que[i]<<" ";
+      
+    }
+    c++; 
+    std::cout << std::endl;
+  }
+  
+
+
+  // for i in set s
+  for(std::set<int>::iterator it = s.begin();it!=s.end();it++){
+    std::set<int> s_next = s;
+    std::deque<int> q_next = que;
+    s_next.erase(*it);
+    q_next.push_back(*it);
+    f(q_next,s_next);
+  }
+
+}
 
 int main(int argc, char* argv[])
 {
   // input is the path of a file.
-  // coor city[100];
+  
+  /*
   std::string path = argv[1];
   std::fstream myfile;
   int index; 
@@ -32,10 +79,13 @@ int main(int argc, char* argv[])
     city.push_back(item);
   }
   myfile.close();
-  
-  //calculate. 
-
-  
+  */
+  //calculate.
+  std::deque<int> q(1,1);
+  int myset[] = {2,3,4,5,6,7,8};
+  std::set<int> s(myset,myset+7);
+  f(q, s);
+  std::cout << c <<std::endl;
 
 
 
