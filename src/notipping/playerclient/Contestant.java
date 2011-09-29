e9f37bdfb5b98511ecc8f981436002f31a7eb832
jingwei gu <sirgujingwei@gmail.com>
2011-09-29 15:33:14 -0400

    hw3

--- src/notipping/playerclient/Contestant.java
+++ src/notipping/playerclient/Contestant.java
@@ -1,6 +1,7 @@
 
 import java.io.*;
 
+
 class Contestant extends NoTippingPlayer {
 	private static BufferedReader br;
 
@@ -9,18 +10,52 @@ class Contestant extends NoTippingPlayer {
 	}
 
 	protected String process(String command) {
-	        System.out.println(command);
+		System.out.println(command);  // print the current state
 		System.out.println("Enter move (position weight): ");
 		try {
-			return br.readLine();
-		} catch (Exception ev) {
+			/** deprecated
+			return br.readLine();  // change!!!!
+			*/
+			
+			// TBD: return some sort of string, i.e. position weight
+			return analyze();
+			
+		} catch (Exception ev) {	
 			System.out.println(ev.getMessage());
 		}
-		return "";			
+		return "";		
+		
+		
 	}
+	
+	/**
+	 * parse the state, and put them into some data structure
+	 * @param command
+	 * **/
+	private void parseState(String command){
+		//TBD
+	}
+	
+	
+	/**
+	 * analyze the next move
+	 * 
+	 * @return string 
+	 * **/
+	protected string analyze(){
+		//TBD
+		
+	}
+	
+	
+	
+	
+	
 
 	public static void main(String[] args) throws Exception {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		new Contestant(Integer.parseInt(args[0]));
+		
+		
 	}
 }
