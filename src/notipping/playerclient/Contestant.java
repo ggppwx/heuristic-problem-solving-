645fb249783bfdf2de2f45737da89b64a1851a69
jingwei gu <sirgujingwei@gmail.com>
2011-09-29 21:06:44 -0400

    hw3

--- src/notipping/playerclient/Contestant.java
+++ src/notipping/playerclient/Contestant.java
@@ -4,13 +4,19 @@ import java.io.*;
 
 class Contestant extends NoTippingPlayer {
 	private static BufferedReader br;
-
-	Contestant(int port) {
+	private int Lsupport; 
+	private int Rsupport;
+	
+	
+    Contestant(int port) {
 		super(port);
-	}
+		Lsupport = -6;
+		Rsupport = 6;
+	}   
 
 	protected String process(String command) {
 		System.out.println(command);  // print the current state
+		parseState(command);
 		System.out.println("Enter move (position weight): ");
 		try {
 			/** deprecated
@@ -34,6 +40,65 @@ class Contestant extends NoTippingPlayer {
 	 * **/
 	private void parseState(String command){
 		//TBD
+        String str;
+        BufferedReader buffer_reader = BufferedReader(new StringReader(command));
+        String delims = "[ ]+";
+        while((str = buffer_reader.readLine()) != null){
+        	String[] tokens = str.split(delims);
+        	if(tokens[0].equals("ADDING")){
+        		flag = 1;  // adding flag 
+        	}else if(tokens[0].equals("REMOVING")){
+        		flag = 2 // removing flag
+        	}else{
+        		int number = Integer.parseInt(tokens[0]);
+        		int pos = Integer.parseInt(tokens[1]);
+        		String color = tokens[2];
+        		int block = Integer.parseInt(tokens[3]);
+           		if (color.equals("Red") && number == 1){
+        			Red[block] = pos;
+        			Board[pos] = block;
+        			Lsupport = Lsupport - block*(pos+3);
+        			Rsupport = Rsupport - block*(pos+1);
+        			
+        		}else if(color.equals("Blue") && number == 1){
+        			Blue[block] = pos;
+        			Board[pos] = block;
+        			Lsupport = Lsupport - block*(pos+3);
+        			Rsupport = Rsupport - block*(pos+1);
+        		}else if(color.equals("Green") && number == 1 ){
+        			Green[block] = pos;
+        			Board[pos] = block; 
+        			Lsupport = Lsupport - block*(pos+3);
+        			Rsupport = Rsupport - block*(pos+1);
+        		}
+
+        	}
+        }
+        // 
+        
+	}   
+		
+	
+	/**
+	 * find all stable combinatio
+	 */
+	private void findStable(int block){
+		//TBD
+		// we know current support value Lsupport Rsupport
+		// check the board.   
+		
+		for (int i = -15;i <= 15; ++i){
+			if(Board[i] == 0){
+				if ((Lsupport - block*(i+3))*(Rsupport - block*(i+1)) <= 0){
+					// no tipping
+					// put i into
+					que.push(i);
+				}
+				
+			}
+		}
+		return que;
+		
 	}
 	
 	
