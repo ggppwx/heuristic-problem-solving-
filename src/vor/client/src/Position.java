91350e75342c9dc75c3c480a696bfd388d3c9f14
jingwei gu <sirgujingwei@gmail.com>
2011-10-25 16:59:44 -0400

    hw5

---
+++ src/vor/client/src/Position.java
@@ -0,0 +1,28 @@
+
+public class Position {
+	private int x;
+	private int y;
+	
+	public Position(int xPos, int yPos) {
+		// TODO Auto-generated constructor stub
+		this.setX(xPos);
+		this.setY(yPos);
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+}
