badbbfe67fcf08c1514ff7803ac3831ac8e930dc
jingwei gu <sirgujingwei@gmail.com>
2011-11-14 00:20:17 -0500

    dating

--- src/dating/DatingClient/src/datingclient/Algorithm.java
+++ src/dating/DatingClient/src/datingclient/Algorithm.java
@@ -1,9 +1,13 @@
 package datingclient;
 
+import static org.junit.Assert.*;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.ListIterator;
 import java.util.regex.Pattern;
+
+import org.junit.Test;
 
 import Jama.Matrix;
 
@@ -12,6 +16,10 @@ public class Algorithm{
 	private int noOfAttributes;
 	private ArrayList<Double> scores;  // score for each candidate
 	private ArrayList<Double[]> values; // value for each candidate
+	
+	public Algorithm(){
+		
+	}
 	
     public Algorithm(int noOfAttributes){
     	this.scores = new ArrayList<Double>(); 
@@ -24,9 +32,11 @@ public class Algorithm{
      *  
      */
     public void readCandidates(String input){
-    	// TODO: store them into an data structure, the format is like:
+    	// store them into an data structure, the format is like:
     	// i.e. [0.9626, 0.5476, 0.6633, 0.2155, 0.5082, 0.47, 0.9267, 0.155, 0.279, 0.9403]:[-0.0554]  
-    	Pattern p = Pattern.compile("");
+    	if(!input.startsWith("[")){
+    		return;
+    	}
     	String[] tokens = input.split(", |\\]|\\[|:");
     	Double[] value = new Double[noOfAttributes];
     	int index = 0;
@@ -45,7 +55,6 @@ public class Algorithm{
 
     public String generateCandidate(){
     	// TODO: main logic is here. 
-    	Matrix a;
 	
     	
     	return "";
@@ -53,12 +62,12 @@ public class Algorithm{
     
     /*
      * pre-process. proceed first 20 records. 
+     * use matrix to calculate a possible weight.
      * */
-    private void preProcess(){
+    private double[] preProcess(){
     	// TODO: put first 20 records into a matrix. 
     	// calculate a weight vector. 
     	// w* = (XT X)-1XT Y, where Y is score vector, X is value matrix 
-
     	Double[][] vs = values.toArray(new Double[20][noOfAttributes]);
     	// Iterator<Double[]> vIt = values.iterator();
     	double[][] firstValueM = new double[20][noOfAttributes]; 
@@ -79,9 +88,100 @@ public class Algorithm{
     	
     	Matrix w = ((x.transpose().times(x)).inverse().times(x.transpose())).times(y);
     	double[][] weight = w.getArray();  // weight is n*1
-    	return;
+    	double[] retWeight = new double[noOfAttributes];
+    	for(int i=0; i<noOfAttributes; ++i){
+    		retWeight[i] = weight[i][0];
+    	}
+    	return retWeight;
+    }
+    
+    /*
+     * use gradient descent algorithm to calculate a possible weight.
+     * it seems now the weight cannot pass validator(sum of positive equals 1...)
+     * 
+     * */
+    private double[] gradientDes(){
+    	// TODO: implement gradient descent algorithm
+    	//	Until some stopping condition 
+		//    Gt = vector of length n, initialized to all 0s
+		//    foreach candidate c
+		//	     dotprod = ∑i xc,i*wt,i
+		//	     diff = dotprod - yc
+		//	     foreach gradient index i
+		//		   Gt,i =  Gt,i + diff * xc,i
+		//    foreach weight index i
+		//	     wt+1,i = wt,i - η*Gt,i
+    	assert(values.size() == scores.size());
+    	double eta = 0.0001; // eta is learning rate.
+    	double[] g = new double[noOfAttributes];
+    	double[] weight = new double[noOfAttributes];
+    	for(int i = 0 ; i<noOfAttributes; ++i){
+    		weight[i] = 1.0/(double)noOfAttributes;
+    	}
+    	int iter = 0;
+    	while(true){ // some stopping condition.
+    		Iterator<Double[]> vIt = values.iterator();
+    		Iterator<Double> sIt = scores.iterator();
+    		while(vIt.hasNext() && sIt.hasNext()){ // for each candidates
+    			Double[] x = vIt.next();
+    			Double y = sIt.next();
+    			double dotPro = dotProduct(convertFromDouble(x),weight);
+    			double diff = dotPro - y;
+    			for(int i = 0; i< noOfAttributes; ++i){ // for each gradient index
+    				g[i] = g[i] + diff * (double)x[i];
+    			}
+    			
+    		}
+    		for(int i = 0; i<noOfAttributes; ++i){ // for each weight index. 
+    			weight[i] = weight[i] - eta*g[i];
+    		}
+    		iter ++;
+    		if(iter == 1000000){
+    			break;
+    		}
+    		
+    		
+    	}
+    	return weight;
+    	
+    }
+    
+    private double[] convertFromDouble(Double[] D){
+    	double[] dou = new double[D.length];
+    	for(int i = 0 ; i<D.length; ++i){
+    		dou[i] = (double)D[i];
+    	}
+    	return dou;
+    }
+    
+    private double dotProduct(double[] x, double[] y){
+    	assert(x.length == y.length);
+    	double sum = 0;
+    	for(int i=0; i<x.length; ++i){
+    		sum +=x[i]*y[i]; 
+    	}
+		return sum;
     }
     
     
+    /*
+     * FOR TEST ONLY:  
+     * test private method
+     * */
+    public double[] convertFromDoubleTest(Double[] D){
+    	return convertFromDouble(D);
+    }
+    
+    public double dotProductTest(double[] x, double[] y){
+    	return dotProduct(x,y);
+    }
+    
+    public double[] preProcessTest(){
+    	return preProcess();
+    }
+    
+    public double[] gradientDesTest(){
+    	return gradientDes();
+    }
 
 }
--- src/dating/DatingClient/src/datingclient/Datingclient.java
+++ src/dating/DatingClient/src/datingclient/Datingclient.java
@@ -19,43 +19,45 @@ public class Datingclient {
     
     public static void main(String[] args) throws IOException {
         
-	 machineName = args[0];
-         port = Integer.parseInt(args[1]);
-         noOfArrtributes = Integer.parseInt(args[2]);
+    	machineName = args[0];
+    	port = Integer.parseInt(args[1]);
+    	noOfArrtributes = Integer.parseInt(args[2]);
          
         socketConnection oS = new socketConnection(machineName, Integer.valueOf(port));
 	
-	Algorithm a = new Algorithm(noOfArrtributes); //init the algorithm
+        Algorithm a = new Algorithm(noOfArrtributes); //init the algorithm
 
         for(int i=0;i<22;i++){
-        System.out.println(oS.socketRead());
-	// TODO: read random candidates from server, read line by line format is:
-	// candidates:
-	// [v1, v2, v3, …..  , vN]:[s1]
-	// [v1, v2, v3, …..  , vN]:[s2]
-	// .. 
-	// give candidate1:
-	a.readCandidates("dummy");
+        	String input = oS.socketRead();
+        	System.out.println(input);
+        	// TODO: read random candidates from server, read line by line format is:
+        	// candidates:
+        	// [v1, v2, v3, …..  , vN]:[s1]
+        	// [v1, v2, v3, …..  , vN]:[s2]
+        	// .. 
+        	// give candidate1:
+        	a.readCandidates(input);
 	
         }
         
         for(int l=0;l<20;l++){
         String temp;
         temp = generateRandomCandidates(noOfArrtributes).getValues().toString();
-	// TODO: generate a candidate here. 
-	temp = a.generateCandidate();
+        // TODO: generate a candidate here. 
+        temp = a.generateCandidate();
 
         oS.socketWrite(temp);
         System.out.println(temp);
-	// temp is something u input. 
-        System.out.println(oS.socketRead());
-	// TODO: read the socre given by server. 
-	// format: [v1, v2, v3, …..., vn]:[s1]
-	a.readCandidates("dummy");
+        // temp is something u input. 
+        String input = oS.socketRead();
+        System.out.println(input);
+        // TODO: read the socre given by server. 
+        // format: [v1, v2, v3, …..., vn]:[s1]
+        a.readCandidates(input);
 	       
         String te = oS.socketRead();
-         System.out.println(te);
-	 // format: give candidate2:
+        System.out.println(te);
+        // format: give candidate2:
         if(te.contains("Bye")){
             break;
         }
@@ -70,25 +72,25 @@ public class Datingclient {
 		Double score;
 		DecimalFormat twoDecimalFormat = new DecimalFormat("#.####");
 		
-                       Candidate candi = new Candidate();
-			score= 0d;
-                        List<Double> oL = new ArrayList<Double>();
-			for(int j=0; j<noOfArrtributes; j++){
-                        Double doubleValue ;    
-                        
-                                while(true){
-                                    doubleValue = doubleGenerator.nextDouble();
-                               if(doubleValue==0.0){
-                                    continue;    
-                                }
-                                    break;
-                               }
-                                doubleValue = Double.valueOf(twoDecimalFormat.format(doubleValue));
-                                oL.add(doubleValue);
+		Candidate candi = new Candidate();
+		score= 0d;
+		List<Double> oL = new ArrayList<Double>();
+		for(int j=0; j<noOfArrtributes; j++){
+			Double doubleValue ;    
+			
+			while(true){
+				doubleValue = doubleGenerator.nextDouble();
+				if(doubleValue==0.0){
+					continue;    
+				}
+				break;
 			}
-                       candi.setValues(oL);
-                       return candi;
+			doubleValue = Double.valueOf(twoDecimalFormat.format(doubleValue));
+			oL.add(doubleValue);
 		}
+		candi.setValues(oL);
+		return candi;
+	}
     
 	
 }
